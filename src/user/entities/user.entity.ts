d60e678ec36c169f20215503fe53ad88fbac4b0f
Ciratoku <edissonman255@gmail.com>
2024-08-16 20:46:23 +0300

    user

---
+++ src/user/entities/user.entity.ts
@@ -0,0 +1,17 @@
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
+import { Dash } from "src/dash/entities/dash.entity";
+import { Comment } from "src/comment/entities/comment.entity";
+
+@Entity()
+export class User {
+  @PrimaryGeneratedColumn()
+  id: number;
+  @Column()
+  email: string;
+  @Column()
+  password: string;
+  @OneToMany(() => Dash, (dash) => dash.user, { onDelete: "CASCADE" })
+  dashes: Dash[];
+  @OneToMany(() => Comment, (comment) => comment.user)
+  comments: Comment[];
+}
