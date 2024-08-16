2a24926c8721390c206756a84e912e2985b1490a
Ciratoku <edissonman255@gmail.com>
2024-08-16 20:45:41 +0300

    app

---
+++ src/app.module.ts
@@ -0,0 +1,32 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { UserModule } from "./user/user.module";
+import { CommentModule } from "./comment/comment.module";
+import { CardModule } from "./card/card.module";
+import { DashModule } from "./dash/dash.module";
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configServer: ConfigService) => ({
+        type: "postgres",
+        host: configServer.get("DB_HOST"),
+        port: configServer.get("DB_PORT"),
+        username: configServer.get("DB_USERNAME"),
+        password: configServer.get("DB_PASSWORD"),
+        database: configServer.get("DB_NAME"),
+        synchronize: true,
+        entities: [__dirname + "/**/*.entity{.js, .ts}"],
+      }),
+      inject: [ConfigService],
+    }),
+    UserModule,
+    CommentModule,
+    CardModule,
+    DashModule,
+  ],
+})
+export class AppModule {}
