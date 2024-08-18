1431b5bdf14faf50505b508e0d731ac2d138b279
Ciratoku <edissonman255@gmail.com>
2024-08-18 15:26:30 +0300

    dash create route

--- src/dash/dash.controller.ts
+++ src/dash/dash.controller.ts
@@ -1,15 +1,30 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
-import { DashService } from './dash.service';
-import { CreateDashDto } from './dto/create-dash.dto';
-import { UpdateDashDto } from './dto/update-dash.dto';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  Req,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
+} from "@nestjs/common";
+import { DashService } from "./dash.service";
+import { CreateDashDto } from "./dto/create-dash.dto";
+import { UpdateDashDto } from "./dto/update-dash.dto";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
-@Controller('dash')
+@Controller("dash")
 export class DashController {
   constructor(private readonly dashService: DashService) {}
 
   @Post()
-  create(@Body() createDashDto: CreateDashDto) {
-    return this.dashService.create(createDashDto);
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe())
+  create(@Body() createDashDto: CreateDashDto, @Req() req) {
+    return this.dashService.create(createDashDto, +req.user.id);
   }
 
   @Get()
@@ -17,18 +32,18 @@ export class DashController {
     return this.dashService.findAll();
   }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
+  @Get(":id")
+  findOne(@Param("id") id: string) {
     return this.dashService.findOne(+id);
   }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateDashDto: UpdateDashDto) {
+  @Patch(":id")
+  update(@Param("id") id: string, @Body() updateDashDto: UpdateDashDto) {
     return this.dashService.update(+id, updateDashDto);
   }
 
-  @Delete(':id')
-  remove(@Param('id') id: string) {
+  @Delete(":id")
+  remove(@Param("id") id: string) {
     return this.dashService.remove(+id);
   }
 }
