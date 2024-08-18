349984234556287dcba4f2b69fc209cf0802da12
Ciratoku <edissonman255@gmail.com>
2024-08-18 23:10:56 +0300

    card service

--- src/card/card.service.ts
+++ src/card/card.service.ts
@@ -1,26 +1,56 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCardDto } from './dto/create-card.dto';
-import { UpdateCardDto } from './dto/update-card.dto';
+import { Injectable, NotFoundException } from "@nestjs/common";
+import { CreateCardDto } from "./dto/create-card.dto";
+import { UpdateCardDto } from "./dto/update-card.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Card } from "./entities/card.entity";
+import { Repository } from "typeorm";
 
 @Injectable()
 export class CardService {
-  create(createCardDto: CreateCardDto) {
-    return 'This action adds a new card';
+  constructor(
+    @InjectRepository(Card) private readonly cardRepository: Repository<Card>
+  ) {}
+
+  // TODO: check if dash with given id exists
+  async create(createCardDto: CreateCardDto) {
+    const newCard = {
+      title: createCardDto.title,
+      description: createCardDto.description,
+      dash: { id: createCardDto.dash.id },
+    };
+    return await this.cardRepository.save(newCard);
   }
 
-  findAll() {
-    return `This action returns all card`;
+  async findOne(id: number) {
+    const card = await this.cardRepository.findOne({
+      where: {
+        id,
+      },
+      relations: {
+        comments: true,
+      },
+    });
+    if (!card) throw new NotFoundException({ message: "No such card" });
+    return card;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} card`;
+  async update(id: number, updateCardDto: UpdateCardDto) {
+    const card = await this.cardRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!card) throw new NotFoundException({ message: "No such card" });
+    return await this.cardRepository.update(id, updateCardDto);
   }
 
-  update(id: number, updateCardDto: UpdateCardDto) {
-    return `This action updates a #${id} card`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} card`;
+  async remove(id: number) {
+    const card = await this.cardRepository.findOne({
+      where: {
+        id,
+      },
+    });
+    if (!card) throw new NotFoundException({ message: "No such card" });
+    return await this.cardRepository.delete(id);
   }
 }
