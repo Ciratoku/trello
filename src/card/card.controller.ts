6c3c6a9f64cb0f384b5f6da1884ea71c27c8d76a
Ciratoku <edissonman255@gmail.com>
2024-08-18 23:10:39 +0300

    controller: routes

--- src/card/card.controller.ts
+++ src/card/card.controller.ts
@@ -6,36 +6,41 @@ import {
   Patch,
   Param,
   Delete,
+  UseGuards,
+  UsePipes,
+  ValidationPipe,
 } from "@nestjs/common";
 import { CardService } from "./card.service";
 import { CreateCardDto } from "./dto/create-card.dto";
 import { UpdateCardDto } from "./dto/update-card.dto";
+import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
 
 @Controller("cards")
 export class CardController {
   constructor(private readonly cardService: CardService) {}
 
   @Post()
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe())
   create(@Body() createCardDto: CreateCardDto) {
     return this.cardService.create(createCardDto);
   }
 
-  @Get()
-  findAll() {
-    return this.cardService.findAll();
-  }
-
   @Get(":id")
+  @UseGuards(JwtAuthGuard)
   findOne(@Param("id") id: string) {
     return this.cardService.findOne(+id);
   }
 
   @Patch(":id")
+  @UseGuards(JwtAuthGuard)
+  @UsePipes(new ValidationPipe())
   update(@Param("id") id: string, @Body() updateCardDto: UpdateCardDto) {
     return this.cardService.update(+id, updateCardDto);
   }
 
   @Delete(":id")
+  @UseGuards(JwtAuthGuard)
   remove(@Param("id") id: string) {
     return this.cardService.remove(+id);
   }
