17c6a9061c8fed1bb4142a84b22ad3edbeab04ee
Ciratoku <edissonman255@gmail.com>
2024-08-19 17:01:02 +0300

    apitags

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -11,7 +11,8 @@ import {
 } from "@nestjs/common";
 import { UserService } from "./user.service";
 import { CreateUserDto } from "./dto/create-user.dto";
-
+import { ApiTags } from "@nestjs/swagger";
+@ApiTags("users")
 @Controller("users")
 export class UserController {
   constructor(private readonly userService: UserService) {}
@@ -22,8 +23,8 @@ export class UserController {
     return this.userService.create(createUserDto);
   }
 
-  @Delete(":id")
-  remove(@Param("id") id: number) {
-    return this.userService.remove(id);
-  }
+  // @Delete(":id")
+  // remove(@Param("id") id: number) {
+  //   return this.userService.remove(id);
+  // }
 }
